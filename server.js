0349f2c993f96dd532e8f3b1b907d4861241aa5e
Andry Pebrianto <andrypeb227@gmail.com>
2022-04-29 11:17:16 +0700

    Configure eslint

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const helmet = require("helmet");
-const xss = require("xss-clean");
-const cors = require("cors");
-const { APP_NAME, NODE_ENV, PORT } = require("./src/utils/env");
-const { failed } = require("./src/utils/createResponse");
+const express = require('express');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const { APP_NAME, NODE_ENV, PORT } = require('./src/utils/env');
+const { failed } = require('./src/utils/createResponse');
 
 // deklarasi express
 const app = express();
@@ -14,24 +14,22 @@ app.use(
   helmet({
     crossOriginEmbedderPolicy: false,
     crossOriginResourcePolicy: false,
-  })
+  }),
 );
 app.use(xss());
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // root router
-app.get("/", (req, res) =>
-  res.send(`${APP_NAME} API - ${NODE_ENV[0].toUpperCase() + NODE_ENV.slice(1)}`)
-);
+app.get('/', (req, res) => res.send(`${APP_NAME} API - ${NODE_ENV[0].toUpperCase() + NODE_ENV.slice(1)}`));
 // main router
 
 // 404 router
 app.use((req, res) => {
   failed(res, {
     code: 404,
-    payload: "Resource on that url not found",
-    message: "Not Found",
+    payload: 'Resource on that url not found',
+    message: 'Not Found',
   });
 });
 
