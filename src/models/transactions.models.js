6ba0811cb3a7da02e46babfda7dd3c1a274c5061
hakamfaza <hakamfaza@gmail.com>
2022-04-30 17:31:45 +0700

    update response

--- src/models/transactions.models.js
+++ src/models/transactions.models.js
@@ -2,7 +2,6 @@ const db = require('../config/db');
 
 const transactionsModels = {
   createTransaction: (setData) => new Promise((resolve, rejectt) => {
-    console.log(setData);
     db.query('INSERT INTO transactions (product_id, is_paid, user_id, seat, id) VALUES ($1, $2, $3, $4, $5)', [setData.product_id, setData.is_paid, setData.seat, setData.user_id, setData.id], (err, result) => {
       if (err) {
         rejectt(err);
