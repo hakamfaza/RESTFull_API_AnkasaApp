b441292150fa28b5aefc60d8f72b9869cfa5329c
hakamfaza <hakamfaza@gmail.com>
2022-04-30 14:16:04 +0700

    add model transactions

---
+++ src/controllers/transactions.controller.js
@@ -0,0 +1,13 @@
+// const {} = require('../models');
+
+const transactionsController = {
+  getTransactions: async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+  },
+};
+
+module.exports = transactionsController;
