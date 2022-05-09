2628350db19c73d76a4d4b9bc22737fa474e38c4
hakamfaza <hakamfaza@gmail.com>
2022-05-10 00:47:57 +0700

    delete jwtAuth

--- src/router/destination.router.js
+++ src/router/destination.router.js
@@ -2,15 +2,14 @@ const express = require('express');
 const {
   creteDestination, getDestination, getDetailDestination, updateDestination, deleteDestination,
 } = require('../controllers/destination.controller');
-const jwtAuth = require('../middlewares/jwtAuth');
 const upload = require('../middlewares/upload');
 
 const router = express.Router();
 
 router
-  .post('/destination', jwtAuth, upload, creteDestination)
-  .get('/destination', jwtAuth, getDestination)
-  .get('/destination/:id', jwtAuth, getDetailDestination)
-  .put('/destination/:id', jwtAuth, upload, updateDestination)
-  .delete('/destination/:id', jwtAuth, deleteDestination);
+  .post('/destination', upload, creteDestination)
+  .get('/destination', getDestination)
+  .get('/destination/:id', getDetailDestination)
+  .put('/destination/:id', upload, updateDestination)
+  .delete('/destination/:id', deleteDestination);
 module.exports = router;
