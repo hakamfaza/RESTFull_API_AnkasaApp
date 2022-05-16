b70e8b679124d48c73c710e8c1b632d0107df84b
Andry Pebrianto <andrypeb227@gmail.com>
2022-05-16 16:11:45 +0700

    Update validation untuk update user

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -8,17 +8,15 @@ const update = [
   }),
   check('name', 'Name maximum length is 50 characters').isLength({ max: 50 }),
   // phone
-  check('phone', 'Phone required').not().isEmpty(),
   check('phone', 'Phone only can contains number').isNumeric(),
   check('phone', 'Phone maximum length is 13 characters').isLength({ max: 13 }),
   // city
-  check('city', 'City required').not().isEmpty(),
   check('city', 'City maximum length is 100 characters').isLength({ max: 100 }),
   // address
-  check('address', 'Address required').not().isEmpty(),
   check('address', 'Address maximum length is 100 characters').isLength({ max: 100 }),
   // postal_code
   check('postalCode', 'Postal Code required').not().isEmpty(),
+  check('postalCode', 'Postal Code only can contains number').isNumeric(),
 ];
 
 module.exports = {
